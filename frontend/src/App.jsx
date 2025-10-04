752bf706eee4c73132f42be3fe7c244134c7d05c
[Sharlyn] <[sharlynbarreto1@gmail.com]>
2025-10-04 16:48:02 -0400

    Front-end with navbar accessibilities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,20 @@
 import './App.css'
 import Navbar from './components/Navbar';
 
+
 function App() {
 
 
   return (
     <>
       <Navbar />
-      <div className="debate-container">
-        {/* Box 1: Camera Display & Controls */}
-        <div className="video-area">
-          {/* ... WebcamDisplay component here ... */}
-          {/* ... ControlPanel component here ... */}
-        </div>
-
-        {/* Box 2: AI Feedback / Chat */}
-        <div className="chat-box">
-          {/* ... AIFeedback component here ... */}
-        </div>
-      </div>
+      <div style={{ 
+      backgroundColor: '#0A0F26', 
+      minHeight: '100vh',
+      width: '100%'
+    }}>
+      
+    </div>
     </>
   );
 }
