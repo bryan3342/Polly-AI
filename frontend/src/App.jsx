da9e5624577728a943034516c4267d21de4aeac5
[Sharlyn] <[sharlynbarreto1@gmail.com]>
2025-10-04 03:51:44 -0400

    Nav Bar implementaion and flex box for chat and camera

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,24 @@ function App() {
 
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <div className="debate-container">
+        {/* Box 1: Camera Display & Controls */}
+        <div className="video-area">
+          {/* ... WebcamDisplay component here ... */}
+          {/* ... ControlPanel component here ... */}
+        </div>
+
+        {/* Box 2: AI Feedback / Chat */}
+        <div className="chat-box">
+          {/* ... AIFeedback component here ... */}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
+
+
+
